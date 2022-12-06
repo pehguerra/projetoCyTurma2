0b79a3ddfd7bfdc111d1f49f6254e3b4766e99e8
Pedro Guerra <pguerra@cambridgeassociates.com>
2022-12-06 19:57:15 -0300

    adicionado o teste de alerta

---
+++ cypress/e2e/ui/conexaoQA_alerta.cy.js
@@ -0,0 +1,31 @@
+describe('alertas', () => {
+    
+    it('valida o alerta de credencial inválida', () => {
+        
+        cy.clock()
+
+        cy.visit('/login')
+
+        // preenche o campo email
+        cy.getElement('login-email')
+            .type('pedro@pedro.com')
+
+        // preenche o campo senha
+        cy.getElement('login-password')
+            .type('123456')
+
+        // clica no botão login
+        cy.getElement('login-submit')
+            .click()
+
+        // valida o alerta de credencial inválida
+        cy.getElement('alert')
+            .should('have.text', 'Credenciais inválidas')
+
+        cy.tick(10000)
+
+        // valida se o alerta desapareceu
+        cy.getElement('alert')
+            .should('not.exist')
+    })
+})
