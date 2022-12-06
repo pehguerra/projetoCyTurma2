b0701ac409b4f3f7c92465b1d5319e6ba03a9047
Pedro Guerra <pguerra@cambridgeassociates.com>
2022-12-06 19:33:52 -0300

    terminado os testes de paginacao

--- cypress/e2e/ui/conexaoQA_paginacao.cy.js
+++ cypress/e2e/ui/conexaoQA_paginacao.cy.js
@@ -12,20 +12,25 @@ describe('paginação da página de QAs', () => {
             .should('not.exist')
     })
 
-    it.only('valida paginação com 8 perfis', () => {
+    ;[
+        { fixture: 'paginacao_8_usuarios', resultadoEsperado: ['<', '1', '2', '>'] },
+        { fixture: 'paginacao_63_usuarios', resultadoEsperado: ['<', '1', '2', '3', '4', '5', '6', '7', '8', '9', '>'] },
+        { fixture: 'paginacao_64_usuarios', resultadoEsperado: ['<', '1', '2', '3', '4', '5', '6', '...', '8', '9', '10', '>'] },
+    ].forEach(({ fixture, resultadoEsperado }) => {
 
-        const resultadoEsperado = ['<', '1', '2', '>']
+        it(`validar a ${fixture}`, () => {
 
-        cy.intercept('GET', '/api/profile', { fixture: 'paginacao_8_usuarios' })
-            .as('perfis')
-
-        cy.visit('/perfis')
-
-        cy.get('.paginationBttns li')
-            .each((el, i) => {
-
-                cy.wrap(el)
-                    .should('have.text', resultadoEsperado[i])
-            })
+            cy.intercept('GET', '/api/profile', { fixture })
+                .as('perfis')
+    
+            cy.visit('/perfis')
+    
+            cy.get('.paginationBttns li')
+                .each((el, i) => {
+    
+                    cy.wrap(el)
+                        .should('have.text', resultadoEsperado[i])
+                })
+        })
     })
 })
